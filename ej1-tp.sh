9c0938f5e8b366d04b5a6543acee233c321dea07
Elias <elu1996biscaia@gmail.com>
2020-04-21 21:02:13 -0300

    actualizado

--- ej1-tp.sh
+++ ej1-tp.sh
@@ -1,30 +1,41 @@
 #!/bin/bash 
 ErrorS() { 
     echo "Error. La sintaxis del script es la siguiente:"
-    echo "......................: $0 nombre_archivo L" # COMPLETAR
-    echo "......................: $0 nombre_archivo C" # COMPLETAR 
-    echo "......................: $0 nombre_archivo M" # COMPLETAR 
+    echo "Cantidad de lineas en el archivo: $0 nombre_archivo L" # COMPLETAR
+    echo "Cantidad de caracteres en el archivo: $0 nombre_archivo C" # COMPLETAR 
+    echo "El tamaño  maximo de las lineas : $0 nombre_archivo M" # COMPLETAR 
     } 
 ErrorP() { 
-    echo "Error.  $1 no se puede leer  " # COMPLETAR
+    echo "Error. El archivo $1 no existe o no tiene permisos de lectura  " # Se indica no se puede leer el archivo 
     }
 
-if  test $# -lt 2 ;then
+if  test $# -lt 2 ;then # Verifica que se ingreso los paramentros correctamente 
      ErrorS
 fi
-if !(test -r $1);then
+if !(test -r $1);then #  Verifica si se puede  leer archivo
     ErrorP 
-elif test -f $1 && (test $2 = "L" || test $2 = "C" || test $2 = "M"); then
+    elif test -f $1 && (test $2 = "L" || test $2 = "C" || test $2 = "M"); then
     if test $2 = "L"; then 
         res=`wc -l $1`
-        echo "La cantidad de linias es : $res" # COMPLETAR
+        echo "La cantidad de linias es : $res" # Si el segundo parametro es igual a L ee cuenta y imprime la cantidad de lineas en el archivo 
     elif test $2 = "C"; then 
         res=`wc -m $1` 
-        echo "La cantidad de char es: $res" # COMPLETAR
+        echo "La cantidad de caracteres es: $res" # Si el segundo parametro es igual a C se cuenta y imprime la cantidad de char en el archivo incluyendo \n
     elif test $2 = "M"; then 
         res=`wc -L $1`
-        echo "La cantidad char en la linea más larga: $res" # COMPLETAR
+        echo "La cantidad de caracteres en la linea más larga: $res" # Si el segundo parametro es igual a M se cuenta y se imprime el tamaño maximo  en el archivo
     fi
 else
      ErrorS 
 fi
+
+
+#
+#
+
+
+#b) El script tiene dos parametros : [Archivo] [Char] 
+# En el primero se indica el archivo a prosesar
+# En el segundo la opcion deseada (L,C,M) 
+#e) La variable "$#" indica la cantidad de parametros ingresados 
+#
