608b72ae1a534e34f96f0389a3119b82d996b461
Elias <elu1996biscaia@gmail.com>
2020-04-29 17:48:59 -0300

    bug fix

--- ej3.sh
+++ ej3.sh
@@ -6,39 +6,8 @@ function help () {
     echo "help" # arguments are accessible through help, echo "$1" # arguments are accessible throughhelp, ,...,...
 }
 
-while getopts t:f:h opt; do
-  case $opt in
-
-    t)
-      DIR=$OPTARG
-      ;;
-    f)
-      SECOND=$OPTARG
-      ;;
-    h)
-      help
-      ;;
-    \?) #unrecognized option - show help
-      echo -e \\n"Option -${BOLD}$OPTARG${OFF} not allowed."
-      HELP
-      ;;
-  esac
-done
-
-
-
-md51 =`ls  | awk '/^.*-[0-9]{1,2}.log$/'|md5sum`
-while(true){
-    md52 =`ls  | awk '/^.*-[0-9]{1,2}.log$/'|md5sum`
-    if(md51 != md52 );then
-        eliminar
-    fi
-    md51=md52
-    sleep $SECOND
-}
-
 function eliminar () {
-    eliminar=$(ls  | awk '/^.*-[0-9]{1,2}.log$/{
+    eliminar=$(ls | sort -V|awk '/^.*-[0-9]{1,2}.log$/{
     n=match($1,/[0-9]{2}/)
     noeliminar[substr($1,0,n-1)]= $1
     eliminar[$1]=0;
@@ -53,5 +22,40 @@ for archivo in $eliminar ; do
     print $archivo
     rm $archivo
 done
-
+echo "elimino log viejos"
 }
+
+while getopts t:f:h opt; do
+  case $opt in
+
+    t)
+      SECOND=$OPTARG
+      ;;
+    f)
+    DIR=$OPTARG
+      ;;
+    h)
+      help
+      ;;
+    \?) #unrecognized option - show help
+      echo -e \\n"Option -${BOLD}$OPTARG${OFF} not allowed."
+      HELP
+      ;;
+  esac
+done
+
+
+
+md51=$(ls | awk '/^.*-[0-9]{1,2}.log$/' | md5sum |awk '{ print $1}')
+
+while(true)
+do
+    md52=$(ls | awk '/^.*-[0-9]{1,2}.log$/' | md5sum |awk '{ print $1}')
+
+    if [ "$md51" != "$md52" ] ; then
+        eliminar
+    fi
+    md51=$md5
+    sleep $SECOND
+done
+
