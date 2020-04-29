c85e705378b6b86382afff24e0eeda5ab6340824
Elias <elu1996biscaia@gmail.com>
2020-04-29 16:49:13 -0300

    comprueba creacion de archivos

--- ej3.sh
+++ ej3.sh
@@ -1,7 +1,44 @@
 #!/bin/bash
 
 
-eliminar=$(ls | awk '/^.*-[0-9]{1,2}.log$/{
+
+function help () {
+    echo "help" # arguments are accessible through help, echo "$1" # arguments are accessible throughhelp, ,...,...
+}
+
+while getopts t:f:h opt; do
+  case $opt in
+
+    t)
+      DIR=$OPTARG
+      ;;
+    f)
+      SECOND=$OPTARG
+      ;;
+    h)
+      help
+      ;;
+    \?) #unrecognized option - show help
+      echo -e \\n"Option -${BOLD}$OPTARG${OFF} not allowed."
+      HELP
+      ;;
+  esac
+done
+
+
+
+md51 =`ls  | awk '/^.*-[0-9]{1,2}.log$/'|md5sum`
+while(true){
+    md52 =`ls  | awk '/^.*-[0-9]{1,2}.log$/'|md5sum`
+    if(md51 != md52 );then
+        eliminar
+    fi
+    md51=md52
+    sleep $SECOND
+}
+
+function eliminar () {
+    eliminar=$(ls  | awk '/^.*-[0-9]{1,2}.log$/{
     n=match($1,/[0-9]{2}/)
     noeliminar[substr($1,0,n-1)]= $1
     eliminar[$1]=0;
@@ -16,3 +53,5 @@ for archivo in $eliminar ; do
     print $archivo
     rm $archivo
 done
+
+}
